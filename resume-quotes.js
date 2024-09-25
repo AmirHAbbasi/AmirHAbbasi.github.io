007bdac8b46f96a79da295217663e545ccba99e0
Amir H.Abbasi♚ Ѧ <83092634+AmirHAbbasi@users.noreply.github.com>
2024-09-25 15:47:32 +0330

    Update resume-quotes.js

--- resume-quotes.js
+++ resume-quotes.js
@@ -10,14 +10,14 @@
 		{
 			"fa":"https://fa.wikipedia.org/wiki/%D9%85%D8%B3%D8%B9%D9%88%D8%AF_%D9%BE%D8%B2%D8%B4%DA%A9%DB%8C%D8%A7%D9%86",
 			"en":"https://en.wikipedia.org/wiki/Masoud_Pezeshkian",
-			"ar":"https://ar.wikipedia.org/wiki/%D9%85%D8%B3%D8%B9%D9%88%D8%AF_%D8%A8%D8%B2%D8%B4%D9%83%D9%8A%D8%A7%D9%86",
+			"ar":"https://ar.wikipedia.org/wiki/%D9%85%D8%B3%D8%B9%D9%88%D8%AF_%D8%A8%D8%B2%D8%B4%D9%83%D9%8A%D8%A7%D9%86"
 		},
 		"image" : "https://upload.wikimedia.org/wikipedia/commons/5/55/Masoud_Pezeshkian_2024_%28A%29_%28cropped%29.jpg",
 		"state" :
 		{
 			"fa":"رژیم صهیونیستی درحال کشتار در غزه است.",
 			"en":"Zionist regime is doing genocide in Gaza!",
-			"ar":"کیان الصهیونی یرتکب الانقطاع فی قطاع غزه",
-		},		
+			"ar":"کیان الصهیونی یرتکب الانقطاع فی قطاع غزه"
+		}
 	}
 ]
